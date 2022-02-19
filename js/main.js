e02ac3f2d84bae6117b917ef8db53251ca1ec85a
Shohbozjon <shohbozjonquvondiqov@gmail.com>
2022-02-19 12:08:35 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var isName = prompt("Ismingizni kiriting") ;
+var isBootstap = prompt("Bootstrapda inputning classi nima?") ;
+var isGutters = prompt("Bootstrapda guttersning vazifasi nima?") ;
+var isModal = prompt("Bootstrapda Modal nima vazifa amalga oshiradi?") ;
+var isFluid = prompt("Container-fluidni tushuntirib bering") ;
+var isExtra = prompt("Extra large necha pixeldan nechi pixelgacha bo'lgan qismni o'z ichiga oladi?") ;
+var isReadonly = prompt("Readonlyni tushuntirib bering") ;
+var isValidation = prompt("Validationni tushuntirib bering") ;
+var isColors = prompt("Bootstrapdagi asosiy ranglarni sanang, (masalan:Dark)") ;
+var isDropdowns = prompt("Dropdowns nima?") ;
+var isRounded = prompt("Rounded nima vazifani amalga oshiradi?") ;
+
+
+console.log( isName + "\n" +isBootstap + "\n" +isGutters +"\n" + isModal + "\n" + isFluid + "\n" + isExtra + "\n" + isReadonly + "\n" + isValidation + "\n" + isColors + "\n" + isDropdowns + "\n" + isRounded )
+
